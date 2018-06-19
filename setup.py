90135a711bf7d9942d10cfbd59218cfbfecf0bd0
gesteves91 <esteves.gean@gmail.com>
2018-06-19 17:25:44 -0300

    add all content files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='This is a sample test',
+    author='gesteves91',
+    license='MIT',
+)
